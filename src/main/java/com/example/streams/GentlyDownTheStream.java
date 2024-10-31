6be02d5370cd52cdb7d4bd8566ca00ec1b41e2ce
wonderchex <jos2493657@maricopa.edu>
2024-10-31 03:03:51 -0700

    Completed GentlyDownTheStream.java

--- src/main/java/com/example/streams/GentlyDownTheStream.java
+++ src/main/java/com/example/streams/GentlyDownTheStream.java
@@ -30,46 +30,79 @@ public class GentlyDownTheStream {
 
     // TODO - return a list of sorted fruits with all fruits filtered out that start with "A"
     public List<String> sortedFruitsException() {
-        return null;
+
+        return fruits.stream()
+                     .filter(x -> !x.startsWith("A"))
+                     .sorted()
+                     .collect(Collectors.toList());
+
     }
 
     // TODO - return a list with the first 2 elements of a sorted list of fruits
     public List<String> sortedFruitsFirstTwo() {
-        return null;
+        return fruits.stream()
+                     .sorted()
+                     .limit(2)
+                     .collect(Collectors.toList());
+
+
     }
 
     // TODO - return a comma separated String of sorted fruits
     public String commaSeparatedListOfFruits() {
-        return null;
+        return fruits.stream()
+                     .sorted()
+                     .collect(Collectors.joining(", "));
     }
 
     // TODO - return a list of veggies that are sorted in reverse (descending) order
     public List<String> reverseSortedVeggies() {
-        return null;
+        return veggies.stream()
+                      .sorted(Comparator.reverseOrder())
+                      .collect(Collectors.toList());
     }
 
     // TODO - return a list of veggies that are sorted in reverse order, and all in upper case
     public List<String> reverseSortedVeggiesInUpperCase() {
-        return null;
+        return veggies.stream()
+                      .sorted(Comparator.reverseOrder())
+                      .map(v -> v.toUpperCase(Locale.ROOT))
+                      .collect(Collectors.toList());
+
     }
 
     // TODO - return a list of the top 10 values in the list of random integers
     public List<Integer> topTen() {
-        return null;
+        return integerValues.stream()
+                            .sorted(Comparator.reverseOrder())
+                            .limit(10)
+                            .collect(Collectors.toList());
     }
 
     // TODO - return a list of the top 10 unique values in the list of random integers
     public List<Integer> topTenUnique() {
-        return null;
+        return integerValues.stream()
+                            .sorted(Comparator.reverseOrder())
+                            .distinct()
+                            .limit(10)
+                            .collect(Collectors.toList());
     }
 
     // TODO - return a list of the top 10 unique values in the list of random integers that are odd
     public List<Integer> topTenUniqueOdd() {
-        return null;
+        return integerValues.stream()
+                            .sorted(Comparator.reverseOrder())
+                            .distinct()
+                            .filter(x -> x % 2 != 0)
+                            .limit(10)
+                            .collect(Collectors.toList());
     }
 
     // TODO - return a single value that represents the average of all of your random numbers
     public Double average() {
-        return null;
+        return integerValues.stream()
+                            .mapToInt(i -> i)
+                            .average()
+                            .getAsDouble();
     }
 }
--- src/test/java/com/example/streams/GentlyDownTheStreamTest.java
+++ src/test/java/com/example/streams/GentlyDownTheStreamTest.java
@@ -10,6 +10,7 @@ import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 class GentlyDownTheStreamTest {
 
     private GentlyDownTheStream stream;
